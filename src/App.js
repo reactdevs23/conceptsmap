5f40f5771c7c91f679d546d53e423ac256b6bcfe
dalimkumar604 <dalimkumar212582@gmail.com>
2024-02-15 23:24:39 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,168 @@
+import { useState } from "react";
+
+import MainComponent from "./components/MainComponent/MainComponent";
+
+function App() {
+  const [allData, setAllData] = useState({
+    fontFamily: "'Poppins', sans-serif",
+    titleFontFamily: "'Ovo', serif",
+    mainBg: "#fff",
+    header: {
+      heading: "Concept Map",
+      headingColor: "#000",
+      border: "3px solid #000",
+      background: "#fff",
+    },
+    ideas: [
+      {
+        mainIdea: {
+          titleContainer: {
+            title: "Main Idea",
+            titleColor: "#000",
+            border: "3px solid #000",
+            background: "#B4F0FF",
+          },
+          info: "orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ",
+          infoColor: "#000",
+          border: "3px solid #000",
+          background: "#fff",
+          boxBorder: "3px solid #000",
+          boxBackground: "#B4F0FF",
+        },
+        secondaryIdea: {
+          titleContainer: {
+            title: "Secondary Idea",
+            titleColor: "#000",
+            border: "3px solid #000",
+            background: "#B4F0FF",
+          },
+          info: "orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ",
+          infoColor: "#000",
+          border: "3px solid #000",
+          background: "#fff",
+          boxBorder: "3px solid #000",
+          boxBackground: "#B4F0FF",
+        },
+        example: {
+          titleContainer: {
+            title: "Example",
+            titleColor: "#000",
+            border: "3px solid #000",
+            background: "#B4F0FF",
+          },
+          info: "orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ",
+          infoColor: "#000",
+          border: "3px solid #000",
+          background: "#fff",
+          boxBorder: "3px solid #000",
+          boxBackground: "#B4F0FF",
+        },
+      },
+      {
+        mainIdea: {
+          titleContainer: {
+            title: "Main Idea",
+            titleColor: "#000",
+            border: "3px solid #000",
+            background: "#F8CFF0",
+          },
+          info: "orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ",
+          infoColor: "#000",
+          border: "3px solid #000",
+          background: "#fff",
+          boxBorder: "3px solid #000",
+          boxBackground: "#F8CFF0",
+        },
+        secondaryIdea: {
+          titleContainer: {
+            title: "Secondary Idea",
+            titleColor: "#000",
+            border: "3px solid #000",
+            background: "#F8CFF0",
+          },
+          info: "orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ",
+          infoColor: "#000",
+          border: "3px solid #000",
+          background: "#fff",
+          boxBorder: "3px solid #000",
+          boxBackground: "#F8CFF0",
+        },
+        example: {
+          titleContainer: {
+            title: "Example",
+            titleColor: "#000",
+            border: "3px solid #000",
+            background: "#F8CFF0",
+          },
+          info: "orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ",
+          infoColor: "#000",
+          border: "3px solid #000",
+          background: "#fff",
+          boxBorder: "3px solid #000",
+          boxBackground: "#F8CFF0",
+        },
+      },
+      {
+        mainIdea: {
+          titleContainer: {
+            title: "Main Idea",
+            titleColor: "#000",
+            border: "3px solid #000",
+            background: "#FFF496",
+          },
+          info: "orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ",
+          infoColor: "#000",
+          border: "3px solid #000",
+          background: "#fff",
+          boxBorder: "3px solid #000",
+          boxBackground: "#FFF496",
+        },
+        secondaryIdea: {
+          titleContainer: {
+            title: "Secondary Idea",
+            titleColor: "#000",
+            border: "3px solid #000",
+            background: "#FFF496",
+          },
+          info: "orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ",
+          infoColor: "#000",
+          border: "3px solid #000",
+          background: "#fff",
+          boxBorder: "3px solid #000",
+          boxBackground: "#FFF496",
+        },
+        example: {
+          titleContainer: {
+            title: "Example",
+            titleColor: "#000",
+            border: "3px solid #000",
+            background: "#FFF496",
+          },
+          info: "orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text  ",
+          infoColor: "#000",
+          border: "3px solid #000",
+          background: "#fff",
+          boxBorder: "3px solid #000",
+          boxBackground: "#FFF496",
+        },
+      },
+    ],
+  });
+
+  return (
+    <>
+      <div
+        style={{
+          "--mainBg": allData.mainBg,
+          "--fontFamily": allData.titleFontFamily,
+
+          fontFamily: allData.fontFamily,
+        }}
+      >
+        <MainComponent {...allData} />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/MainComponent/MainComponent.js
@@ -0,0 +1,114 @@
+import React from "react";
+import classes from "./MainComponent.module.css";
+
+const MainComponent = ({ header, ideas }) => {
+  return (
+    <div
+      className={`min-h-screen flex py-10 items-center flex-col	 justify-center w-full ${classes.mainWrapper}`}
+      style={{ background: "var(--mainBg)" }}
+    >
+      <div
+        className={classes.header}
+        style={{
+          "--color": header.headingColor,
+          "--bg": header.background,
+          "--border": header.border,
+        }}
+      >
+        <h2 className={classes.heading}>{header.heading}</h2>
+      </div>
+      <div className={classes.ideas}>
+        {ideas.map(({ mainIdea, secondaryIdea, example }, i) => (
+          <div className={classes.ideaContainer}>
+            <div
+              className={[classes.mainIdea, classes.idea].join(" ")}
+              style={{
+                "--border": mainIdea.border,
+                "--bg": mainIdea.background,
+                "--boxBorder": mainIdea.boxBorder,
+                "--boxBg": mainIdea.boxBackground,
+              }}
+            >
+              <div
+                className={classes.titleContainer}
+                style={{
+                  "--border": mainIdea.titleContainer.border,
+                  "--bg": mainIdea.titleContainer.background,
+                  "--color": mainIdea.titleContainer.titleColor,
+                }}
+              >
+                <h4 className={classes.title}>
+                  {mainIdea.titleContainer.title}
+                </h4>
+              </div>
+              <p
+                className={classes.info}
+                style={{ "--color": mainIdea.infoColor }}
+              >
+                {mainIdea.info}
+              </p>
+            </div>{" "}
+            <div
+              className={[classes.secondaryIdea, classes.idea].join(" ")}
+              style={{
+                "--border": secondaryIdea.border,
+                "--bg": secondaryIdea.background,
+                "--boxBorder": secondaryIdea.boxBorder,
+                "--boxBg": secondaryIdea.boxBackground,
+              }}
+            >
+              <div
+                className={classes.titleContainer}
+                style={{
+                  "--border": secondaryIdea.titleContainer.border,
+                  "--bg": secondaryIdea.titleContainer.background,
+                  "--color": secondaryIdea.titleContainer.titleColor,
+                }}
+              >
+                <h4 className={classes.title}>
+                  {secondaryIdea.titleContainer.title}
+                </h4>
+              </div>
+              <p
+                className={classes.info}
+                style={{ "--color": secondaryIdea.infoColor }}
+              >
+                {secondaryIdea.info}
+              </p>
+            </div>{" "}
+            <div
+              className={[classes.example, classes.idea].join(" ")}
+              style={{
+                "--border": example.border,
+                "--bg": example.background,
+                "--boxBorder": example.boxBorder,
+                "--boxBg": example.boxBackground,
+              }}
+            >
+              <div
+                className={classes.titleContainer}
+                style={{
+                  "--border": example.titleContainer.border,
+                  "--bg": example.titleContainer.background,
+                  "--color": example.titleContainer.titleColor,
+                }}
+              >
+                <h4 className={classes.title}>
+                  {example.titleContainer.title}
+                </h4>
+              </div>
+              <p
+                className={classes.info}
+                style={{ "--color": example.infoColor }}
+              >
+                {example.info}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default MainComponent;
